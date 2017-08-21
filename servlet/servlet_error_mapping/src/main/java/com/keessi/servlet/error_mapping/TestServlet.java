28cec844f3fa22c12b82d12bdcfc2d2a2b13504e
SPC1996 <1594475695@qq.com>
2017-08-21 14:49:47 +0800

    2017-08-21 14:49 -- 添加 servlet/error_mapping

---
+++ servlet/servlet_error_mapping/src/main/java/com/keessi/servlet/error_mapping/TestServlet.java
@@ -0,0 +1,28 @@
+package com.keessi.servlet.error_mapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "servlet throw RuntimeException";
+    }
+}
